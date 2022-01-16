42cca5bfd9606d29edbd5380041e41fbc5c3ba39
Yuta Okuma <y.dream.nest@icloud.com>
2022-01-16 21:31:19 +0900

    各GETメソッドの正常系を作成

--- app/app.js
+++ app/app.js
@@ -1,11 +1,43 @@
+// 必要パッケージのインポート
 const express = require('express');
 const app = express();
+const sqlite3 = require('sqlite3');
+const dbPath = 'app/db/database.sqlite3'
 
-// GETメソッド
-app.get('/api/v1/hello', (req, res) => {
-    res.json({
-        "message": "Hello, World!"
+// GETメソッド（Get all users）
+app.get('/api/v1/users', (req, res) => {
+    // データベースに接続する
+    const db = new sqlite3.Database(dbPath);
+
+    db.all('SELECT * FROM users', (err, rows) => {
+        res.json(rows);
     });
+
+    db.close();
+});
+
+// GETメソッド（Get a user）
+app.get('/api/v1/users/:id', (req, res) => {
+    const db = new sqlite3.Database(dbPath);
+    const id = req.params.id;
+
+    db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+        res.json(row);
+    });
+
+    db.close();
+});
+
+// GETメソッド（Search users with matching keyword）
+app.get('/api/v1/search', (req, res) => {
+    const db = new sqlite3.Database(dbPath);
+    const keyword = req.query.q;
+
+    db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
+        res.json(rows);
+    });
+
+    db.close();
 });
 
 // 環境変数に記述されているポート番号を参照し、なければ3000に設定する
